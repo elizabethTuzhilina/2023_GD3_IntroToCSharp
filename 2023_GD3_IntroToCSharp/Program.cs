72394a3e903607b3a1ee1a58257e721da9eab048
Niall McGuinness <niall.mcguinness@dkit.ie>
2023-09-20 09:17:56 +0100

    Add project files.

---
+++ 2023_GD3_IntroToCSharp/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _2023_GD3_IntroToCSharp
+{
+    /// <summary>
+    /// Demo code to introduce core C# concepts useful within the module
+    /// </summary>
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //see README for content description
+        }
+    }
+}
