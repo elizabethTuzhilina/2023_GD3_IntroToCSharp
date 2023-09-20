929f2f612e9067214868513e4f91160077550380
Niall McGuinness <niall.mcguinness@dkit.ie>
2023-09-20 12:30:33 +0100

    :sparkles: Added ref and pass by value demo

---
+++ 2023_GD3_IntroToCSharp/Examples/Classes/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GD.Examples.Classes
+{
+    internal class Player
+    {
+    }
+}
+
+namespace GD
+{
+    namespace Core
+    {
+        public class AI
+        {
+            //some methods
+        }
+
+        namespace Graphics
+        {
+            public class Render3D
+            {
+                //some methods
+            }
+        }
+    }
+}
--- 2023_GD3_IntroToCSharp/Program.cs
+++ 2023_GD3_IntroToCSharp/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using GD.Core.Graphics;
+using System;
+using System.Security.Permissions;
 
-namespace _2023_GD3_IntroToCSharp
+namespace GD
 {
     /// <summary>
     /// Demo code to introduce core C# concepts useful within the module
@@ -10,6 +12,25 @@ namespace _2023_GD3_IntroToCSharp
         private static void Main(string[] args)
         {
             //see README for content description
+            Render3D render = new Render3D();
+            new Program().Start();
+        }
+
+        public void Start()
+        {
+            int a = 10;
+            int b = 50;
+            Swap(ref a, ref b);
+            // Console.WriteLine("a: " + a + "," + "b: " + b);
+            Console.WriteLine($"a:{a}, b:{b} and sum is {a + b}");
+            Console.ReadLine();
+        }
+
+        public void Swap(ref int x, ref int y)
+        {
+            int temp = y;
+            y = x;
+            x = temp;
         }
     }
 }
