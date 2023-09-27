de9a5254e9e679bb9e97161ceea429c64963fe28
Niall McGuinness <niall.mcguinness@dkit.ie>
2023-09-27 11:47:03 +0100

    :sparkles: Added Abstract design pattern demo

---
+++ 2023_GD3_IntroToCSharp/Examples/Design Patterns/Creational/Abstract/IGenerateRoute.cs
@@ -0,0 +1,30 @@
+﻿using GD.Examples.Classes;
+using System;
+using System.Collections.Generic;
+
+namespace GD.Interfaces
+{
+    //an interface is ABSTRACT - we never instanciate - it has no VARS and NO METHOD BODIES
+    public interface IGenerateRoute
+    {
+        List<Vector3> GetRoute(Vector3 start, Vector3 end);
+    }
+
+    public class GenerateDrivingRoute : IGenerateRoute
+    {
+        public List<Vector3> GetRoute(Vector3 start, Vector3 end)
+        {
+            Console.WriteLine("Getting driving interface...");
+            return null;
+        }
+    }
+
+    public class GenerateWalkingRoute : IGenerateRoute
+    {
+        public List<Vector3> GetRoute(Vector3 start, Vector3 end)
+        {
+            Console.WriteLine("Getting walking interface...");
+            return null;
+        }
+    }
+}
